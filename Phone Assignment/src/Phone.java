cc1ca13bcc172a898cbea274ca4c9676d48bc907
Nawal-Dweik <76469553+Nawal-Dweik@users.noreply.github.com>
2021-01-09 01:46:45 +0200

    Add files via upload

---
+++ Phone Assignment/src/Phone.java
@@ -0,0 +1,33 @@
+public abstract class Phone {
+    private String versionNumber;
+    private int batteryPercentage;
+    private String carrier;
+    private String ringTone;
+    public Phone(String versionNumber, int batteryPercentage, String carrier, String ringTone){
+        this.setVersionNumber(versionNumber);
+        this.batteryPercentage = batteryPercentage;
+        this.setCarrier(carrier);
+        this.setRingTone(ringTone);
+    }
+   
+    public abstract void displayInfo();
+    
+	public String getCarrier() {
+		return carrier;
+	}
+	public void setCarrier(String carrier) {
+		this.carrier = carrier;
+	}
+	public String getVersionNumber() {
+		return versionNumber;
+	}
+	public void setVersionNumber(String versionNumber) {
+		this.versionNumber = versionNumber;
+	}
+	public String getRingTone() {
+		return ringTone;
+	}
+	public void setRingTone(String ringTone) {
+		this.ringTone = ringTone;
+	}
+}
